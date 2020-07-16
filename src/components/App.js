84f7ee6a0a88935379e29a68e6f1ce2b7c3350a2
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-07-16 18:48:39 +0800

    remove redundent code and resize elements

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.scss';
 const arr = ['7', '8', '9', '4', '5', '6', '1', '2', '3'
 ];
-function App() {
+function Calculator() {
   return (
-    <div className="App col-xs-11 col-sm-6 col-md-7 my-auto mx-auto">
+    <div className="Calculator col-xs-11 col-sm-6 col-md-6 my-auto mx-auto">
       <div className='container-fluid'>
               <div className='row'>
         <div id='display' className='display col-12 mx-auto'>0</div>
@@ -13,19 +13,19 @@ function App() {
 
   
 
-        <Butt />
+        <Keys />
 
     </div>
   );
 }
 
-function Butt() {
+function Keys() {
   return (
     <div className='keys-container container-fluid'>
 
 
     <div className='container-fluid'>
-            <div className='parentrow row justify-content-center'>
+            <div className='row justify-content-center'>
         <div type='button' className='clear col-5 my-1 mr-2 p-2'>AC</div>
         <div type='button' className='operator-division col-3 m-2 my-2 p-1 mr-5'>/</div>
 
@@ -35,7 +35,7 @@ function Butt() {
 
       <div className='row ml-1 g-0'>
 
-        <div className='box-1 col-9'>
+        <div className='col-9'>
           <div className='row row-cols-4 g-0 justify-content-center'>
             {arr.map((val, i) => (
               <div key={i} type='button' className='btn col-3 m-2'>{val}</div>
@@ -49,12 +49,12 @@ function Butt() {
 
         </div>
 
-        <div className='box-2 col-3'>
+        <div className='col-3'>
 
-          <div type='button' className='operator m-2'>x</div>
-          <div type='button' className='operator m-2'>-</div>
-          <div type='button' className='operator m-2'>+</div>
-          <div type='button' className='equal'>=</div>
+          <div type='button' className='operators m-2'>x</div>
+          <div type='button' className='operators m-2'>-</div>
+          <div type='button' className='operators m-2'>+</div>
+          <div type='button' className='equal d-flex justify-content-center align-items-center'>=</div>
 
 
         </div>
@@ -64,4 +64,4 @@ function Butt() {
   );
 }
 
-export default App;
+export default Calculator;
