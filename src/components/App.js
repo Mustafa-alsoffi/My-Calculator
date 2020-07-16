1c2d901d80154fc621da6d83b66e3fbbee7576d3
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-07-16 14:22:06 +0800

    center elements and some adjustment

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,16 @@ const arr = ['7', '8', '9', '4', '5', '6', '1', '2', '3'
 ];
 function App() {
   return (
-    <div className="App col-7 my-auto mx-auto">
+    <div className="App col-xs-11 col-sm-6 col-md-7 my-auto mx-auto">
       <div className='container-fluid'>
               <div className='row'>
-        <div id='display' className='display col-12'>0</div>
+        <div id='display' className='display col-12 mx-auto'>0</div>
       </div>
       </div>
 
-        
-      
-      
-      
+  
 
         <Butt />
-
-
-      
-
 
     </div>
   );
@@ -31,12 +24,10 @@ function Butt() {
     <div className='keys-container container-fluid'>
 
 
-      {/* className='num-buttons col-4 m-auto justify-content-start' */}
-
     <div className='container-fluid'>
-            <div className='parentrow row'>
-        <div type='button' className='clear col-5 my-1 p-2'>AC</div>
-        <div type='button' className='operator-division col-3 m-1 my-2 p-1'>/</div>
+            <div className='parentrow row justify-content-center'>
+        <div type='button' className='clear col-5 my-1 mr-2 p-2'>AC</div>
+        <div type='button' className='operator-division col-3 m-2 my-2 p-1 mr-5'>/</div>
 
       </div>
     </div>
@@ -45,27 +36,24 @@ function Butt() {
       <div className='row ml-1 g-0'>
 
         <div className='box-1 col-9'>
-          <div className='row row-cols-4 g-0'>
+          <div className='row row-cols-4 g-0 justify-content-center'>
             {arr.map((val, i) => (
-              <div key={i} type='button' className='btn col m-1'>{val}</div>
+              <div key={i} type='button' className='btn col-3 m-2'>{val}</div>
 
             ))}
-            <div type='button' className='zero btn col m-1'><span>0</span></div>
+            <div type='button' className='zero btn col m-2'><span>0</span></div>
 
-            <div type='button' className='btn col m-1'>.</div>
+            <div type='button' className='btn col-3 m-2'>.</div>
           </div>
-
-
-
 
 
         </div>
 
-        <div className='box-2 col-3 '>
+        <div className='box-2 col-3'>
 
-          <div type='button' className='operator m-1'>x</div>
-          <div type='button' className='operator m-1'>-</div>
-          <div type='button' className='operator m-1'>+</div>
+          <div type='button' className='operator m-2'>x</div>
+          <div type='button' className='operator m-2'>-</div>
+          <div type='button' className='operator m-2'>+</div>
           <div type='button' className='equal'>=</div>
 
 
